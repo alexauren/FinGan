cbfdbd020fe9f10916a8b62d9c2df5b3705bd430
alexauren <alexander.auren@gmail.com>
2024-12-13 15:41:21 +0900

    fix issues with outdated libraries

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
-from tensorflow import set_random_seed
+from tensorflow import random
 from fin_data import data_generator
 from keras.models import Sequential
-from keras.layers.merge import Add
+from keras.layers import Add
 from keras.optimizers import Adam,Nadam
 from models import discriminator_model, generator_model_cnn,generator_model_mlp,generator_model_mlp_cnn,generator_model_mlp_cnn_plus
 import matplotlib.pyplot as plt
@@ -15,7 +15,6 @@ from datetime import datetime as dt
 import os
 from shutil import copyfile
 import argparse 
-
 parser = argparse.ArgumentParser(description='FIN-GAN implementation')
 parser.add_argument('--batch_size',type=int,default=24)
 parser.add_argument('--generator_model',type=str,default='mlp-cnn')
@@ -29,7 +28,7 @@ parser.add_argument('--seed',type=int,default=1)
 
 args = parser.parse_args()
 seed(args.seed)
-set_random_seed(args.seed)
+random.set_seed(args.seed)
 
 dg = data_generator()
 batch_size = 24
@@ -65,17 +64,17 @@ def train():
         sys.exit()
     #preparing discriminator
                                                       
-    statistics_opt = Adam(lr=0.0001)
+    statistics_opt = Adam(learning_rate=0.0001)
     generator_statistics.compile(loss='mean_squared_error',optimizer=statistics_opt)
 
     discriminator = discriminator_model()
-    d_opt = Adam(lr=args.discriminator_lr, beta_1=0.1)
+    d_opt = Adam(learning_rate=args.discriminator_lr, beta_1=0.1)
     discriminator.compile(loss='binary_crossentropy', optimizer=d_opt)
     discriminator.trainable = False
     for e in discriminator.layers:
         e.trainable = False
     gan = Sequential([generator,discriminator])
-    g_opt = Adam(lr=args.generator_lr, beta_1=0.5)
+    g_opt = Adam(learning_rate=args.generator_lr, beta_1=0.5)
     gan.compile(loss='binary_crossentropy', optimizer=g_opt)
     
     g_loss_recorder = []
@@ -83,6 +82,7 @@ def train():
     g_losses_recorder = []
     d_losses_recorder = []
     #start training
+    discriminator.compile(loss='binary_crossentropy', optimizer=d_opt)
     for epoch in range(args.epochs):
         for index in range(args.batches):
             noise = np.array([np.random.normal(0,1.0,size=100) for _ in range(batch_size)])
@@ -104,10 +104,10 @@ def train():
             y = np.concatenate([np.random.uniform(0.9,1.1,batch_size),np.random.uniform(0.1,0.3,batch_size)])
             d_loss = discriminator.train_on_batch(X, y)
             d_loss_recorder.append(d_loss)
-            # update generator
+            # update generatorx
             y = np.array([1.]*batch_size,dtype=np.float32)
             g_loss = gan.train_on_batch(noise, y)
             g_loss_recorder.append(g_loss)
             print("epoch: %d, batch: %d, g_loss: %f, d_loss: %f" % (epoch, index, g_loss, d_loss))
-            generator.save_weights('./weights/%s/generator_%i_%i.h5'%(timestamp,epoch,index))
+            generator.save_weights('./weights/%s/generator_%i_%i.weights.h5'%(timestamp,epoch,index))
 train()
